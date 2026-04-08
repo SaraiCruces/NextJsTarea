eb97a411f891013e6adca45d9275c73e70b4a86f
SaraiCruces <saraicruces158@gmail.com>
2026-04-08 02:38:28 +0000

    Seperacion de Layout

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,18 @@
+import './ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <h1>Holap</h1>
+        <div style={{border: '1px solid #000'}}>
+        {children}
+        </div>
+        </body>
+    </html>
+  );
+}
